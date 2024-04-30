d07c8f704f71c89ca92d52a517a027f6786ea329
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-04-30 08:54:48 +0200

    anslutningen till MongoDB databasen fungerar

--- routes/authlogRoutes.js
+++ routes/authlogRoutes.js
@@ -2,6 +2,17 @@
 
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//connect to MongoDB database
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Connected to MongoDB...");
+}).catch((error) => {
+console.error("Error connecting to database");
+});
+
 
 //Add new user
 router.post("/register", async (req, res) => {
